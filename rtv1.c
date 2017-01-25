9f61691140fdc01c196b851d908927294823343b
keuhdall <lmarques@student.42.fr>
2017-01-25 20:50:31 +0100

    added a few tests for the main

--- rtv1.c
+++ rtv1.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 01:48:29 by lmarques          #+#    #+#             */
-/*   Updated: 2017/01/25 01:22:53 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/01/25 20:49:09 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,30 @@
 
 int	main(int argc, char *argv[])
 {
-	int		err;
-	t_env	env;
+	int			err;
+	t_env		env;
+	t_obj_lst	*tmp;
 
 	err = 0;
 	ft_init_tabs(&env);
+	tmp = env.obj_lst;
 	if (argc != 2)
 	{
 		ft_putendl("error");
 		return (-1);
 	}
 	ft_read_file(argv[1], &env, &err);
-	if (ft_check_all(&env) && env.scene.name)
+	if (ft_check_all(&env))
+	{
 		printf("scene name : %s\n", env.scene.name);
+		printf("scene size.x : %d\n", env.scene.size.x);
+		printf("scene size.y : %d\n", env.scene.size.y);
+		while (tmp)
+		{
+			printf("color = %d\n", tmp->obj.color);
+			tmp = tmp->next;
+		}
+	}
 	else if (err)
 	{
 		ft_putendl("error0");
