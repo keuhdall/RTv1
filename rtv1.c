86c180d438e641ffad2da2a66d039f434a301e8b
keuhdall <lmarques@student.42.fr>
2017-01-26 20:16:58 +0100

    enhanced main test

--- rtv1.c
+++ rtv1.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 01:48:29 by lmarques          #+#    #+#             */
-/*   Updated: 2017/01/25 23:06:12 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/01/26 19:13:22 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,36 @@ int	main(int argc, char *argv[])
 	ft_read_file(argv[1], &env, &err);
 	if (ft_check_all(&env))
 	{
+		printf("=====SCENE=====\n");
 		printf("scene name : %s\n", env.scene.name);
 		printf("scene size.x : %d\n", env.scene.size.x);
 		printf("scene size.y : %d\n", env.scene.size.y);
+		printf("===============\n");
+
+		printf("====CAMERA====\n");
+		printf("camera position.x : %f\n", env.camera.position.x);
+		printf("camera position.y : %f\n", env.camera.position.y);
+		printf("camera position.z : %f\n", env.camera.position.z);
+		printf("\n");
+		printf("camera rotation.x : %f\n", env.camera.rotation.x);
+		printf("camera rotation.y : %f\n", env.camera.rotation.y);
+		printf("camera rotation.z : %f\n", env.camera.position.z);
+		printf("==============\n");
+		printf("\n");
 		tmp = env.obj_lst;
 		while (tmp)
 		{
+			printf("=====NEW OBJECT=====\n");
+			printf("object position.x : %f\n", tmp->obj.position.x);
+			printf("object position.y : %f\n", tmp->obj.position.y);
+			printf("object position.z : %f\n", tmp->obj.position.z);
+			printf("\n");
+			printf("object rotation.x : %f\n", tmp->obj.rotation.x);
+			printf("object rotation.y : %f\n", tmp->obj.rotation.y);
+			printf("object rotation.z : %f\n", tmp->obj.position.z);
 			printf("color = %d\n", tmp->obj.color);
+			printf("====================\n");
+			printf("\n");
 			tmp = tmp->next;
 		}
 	}
