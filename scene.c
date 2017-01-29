0a307c3fd24a5b3a0b2b8820ef5ce70b4f13bd5b
Léonard Marques <lmarques@student.42.fr>
2017-01-29 14:45:21 +0100

    added a maximum size to the scene

--- scene.c
+++ scene.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 20:53:58 by lmarques          #+#    #+#             */
-/*   Updated: 2017/01/27 18:49:25 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/01/29 14:44:04 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	ft_fill_scene_size(t_env *env, char **tab)
 	if (!ft_strcmp(tab[0], "size.x"))
 	{
 		env->scene_filled[0] = 1;
-		env->scene.size.x = ft_atoi(tab[1]);
+		env->scene.size.x = ft_atoi(tab[1])  > 1920 ? 1920 : ft_atoi(tab[1]);
 	}
 	else if (!ft_strcmp(tab[0], "size.y"))
 	{
 		env->scene_filled[1] = 1;
-		env->scene.size.y = ft_atoi(tab[1]);
+		env->scene.size.y = ft_atoi(tab[1]) > 1080 ? 1080 : ft_atoi(tab[1]);
 	}
 	else
 		ft_puterr(ERR_FILE_SYNTAX);
