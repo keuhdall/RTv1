191a4c538bfdcf4d6b039cc5ccab5322de79d827
keuhdall <lmarques@student.42.fr>
2017-02-14 21:16:17 +0100

    added spot intensity

--- spot.c
+++ spot.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/06 19:39:53 by lmarques          #+#    #+#             */
-/*   Updated: 2017/02/06 22:39:36 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/02/13 17:00:43 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,17 @@ void	ft_fill_spot_position(t_env *env, char **tab, t_spot *spot, char *found)
 	{
 		spot->position.z = ft_atof(tab[1]);
 		env->spot_filled[2] = 1;
+		*found = 1;
+	}
+}
+
+void	ft_fill_spot_intensity(t_env *env, char **tab, t_spot *spot,
+	char *found)
+{
+	if (!ft_strcmp(tab[0], "intensity"))
+	{
+		spot->intensity = ft_atof(tab[1]);
+		env->spot_filled[3] = 1;
 		*found = 1;
 	}
 }
