38f9087133e9bad3e0eb02cfcba6d47cfb211df5
keuhdall <lmarques@student.42.fr>
2017-02-16 22:55:16 -0800

    removed useless function

--- rt_plane.c
+++ rt_plane.c
@@ -6,31 +6,26 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/06 14:13:51 by lmarques          #+#    #+#             */
-/*   Updated: 2017/02/16 10:46:37 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/02/16 18:34:05 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
-
-t_dpoint_3d	ft_v_unit(t_dpoint_3d v)
-{
-	return (ft_vquo(v, sqrt(ft_dotprod(v, v))));
-}
 
 int			ft_plane_intersec(t_object *obj, t_ray ray)
 {
 	double	t;
 	double	v;
 
-	v = ft_dotprod(ray.dir, ft_v_unit(obj->rotation));
+	v = ft_dotprod(ray.dir, ft_normalize(obj->rotation));
 	if (!v)
 		return (0);
 	t = -(ft_dotprod(ft_vdiff_s(ray.orig, obj->position),
-		ft_v_unit(obj->rotation)) / v);
+		ft_normalize(obj->rotation)) / v);
 	if (t < 0)
 		return (0);
 	obj->intersec = ft_vsum_s(ray.orig, ft_vprod(ray.dir, t));
-	obj->normal = v < 0 ? ft_v_unit(obj->rotation) : ft_vnegative(ft_v_unit(
-		obj->rotation));
+	obj->normal = v < 0 ? ft_normalize(obj->rotation) : ft_vnegative(
+		ft_normalize(obj->rotation));
 	return (1);
 }
