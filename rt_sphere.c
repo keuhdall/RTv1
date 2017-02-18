eb17837aeb939947dd22ff63941d457d84a571f7
keuhdall <lmarques@student.42.fr>
2017-02-17 19:19:35 -0800

    corrected shadow in sphere equation

--- rt_sphere.c
+++ rt_sphere.c
@@ -6,13 +6,13 @@
 /*   By: lmarques <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/28 12:46:18 by lmarques          #+#    #+#             */
-/*   Updated: 2017/02/17 10:03:42 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/02/17 19:18:31 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-static double		ft_solve_sphere(t_dpoint_3d eq, double delta)
+static double		ft_solve_sphere(t_dpoint_3d eq, double delta, char *boolean)
 {
 	t_dpoint_3d	t;
 
@@ -24,7 +24,17 @@ static double		ft_solve_sphere(t_dpoint_3d eq, double delta)
 		t.x = (-eq.y + delta);
 		t.y = (-eq.y - delta);
 		if (t.x < 0 || t.y < 0)
-			return (0);
+		{
+			if (t.x > 0)
+				return (t.x);
+			else if (t.y > 0)
+				return (t.y);
+			else
+			{
+				*boolean = 1;
+				return (0);
+			}
+		}
 		t.z = (t.x < t.y ? t.x : t.y);
 	}
 	return (t.z);
@@ -35,7 +45,10 @@ int					ft_sphere_intersec(t_object *obj, t_ray ray)
 	t_dpoint_3d	p;
 	t_dpoint_3d	px;
 	double		delta;
+	double		t;
+	char		boolean;
 
+	boolean = 0;
 	px = ft_vdiff_s(ray.orig, obj->position);
 	p.y = ft_dotprod(px, ray.dir);
 	p.z = ft_dotprod(px, px) - (obj->size * obj->size);
@@ -44,8 +57,11 @@ int					ft_sphere_intersec(t_object *obj, t_ray ray)
 		return (0);
 	else
 	{
+		t = ft_solve_sphere(p, delta, &boolean);
+		if (boolean)
+			return (0);
 		obj->intersec = ft_vsum_s(ray.orig, ft_vprod(ray.dir,
-			ft_solve_sphere(p, delta)));
+			t));
 		obj->normal = ft_vquo(ft_vdiff_s(obj->intersec, obj->position),
 			obj->size);
 		return (1);
